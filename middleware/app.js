303929afd793adf1636e3b24a6c6219abf100614
swethatharakiran <swethakh29@gmail.com>
2022-12-19 14:51:17 +0530

    add-product and parse req

--- middleware/app.js
+++ middleware/app.js
@@ -1,13 +1,24 @@
 const express=require('express');
+const bodyparser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
     console.log('In the middleware');
-    next();
+    res.send('<form action="/product" method="POST"><label>product name</label><input type="text" name="title"><label>size</label><input type="text" name="size"><button type="submit">Add Product</button></form>');
+    
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>')
-})
+
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    console.log('In another middleware2');
+    res.send('<h1>Hello from Express</h1>');
+});
 
 app.listen(4000);
